b08bf5a62f7ee85b1242d6559a3d7a70737b2b36
Nikunj Maheshwari <nmaheshwari2904@gmail.com>
2023-08-28 11:40:46 +0530

    Class and objects 2

--- a.cpp
+++ a.cpp
@@ -3,69 +3,26 @@ using namespace std;
 
 class student{
     public:
-    string name;
-    int age;
-    bool gender;
+    int number;
+    float price;
 
-    //creating a function of student class
+    void getdata(int a , float b);
+
     void printinfo(){
-        cout<<endl<<"Name: "<<name<<endl;
-        cout<<"Age: "<<age<<endl;
-        if(gender == 0){
-            cout<<"Gender: Female"<<endl;
-        }
-        if(gender == 1){
-            cout<<"Gender: Male"<<endl;
-        }
+        cout<<number<<", ";
+        cout<<price;
     }
+}x,y,z;
 
-    //creating a parameterised constructor function of student class
-    student(string s,int a, int g){
-        cout<<"Parameterised Constructor."<<endl;
-        name = s;
-        age = a;
-        gender = g;
-    }
-
-    //default constructor function of student class
-    student(){
-        cout<<"Default Constructor."<<endl;
-    }
-
-    //copy constructor
-    student(student &a){
-        cout<<"Copy constructor."<<endl;
-        name = a.name;
-        age = a.age;
-        gender = a.gender;
-        cout<<"Name: "<<name<<endl;
-        cout<<"Age: "<<age<<endl;
-        cout<<"Gender: "<<gender<<endl;
-    }
-};
+inline void student::getdata(int a, float b){
+    number = a;
+    price = b;
+}
 
 int main(){
-    student a("Nikunj",19,1);
-    student b("Priyanka",19,0);
-    // a.printinfo();
-    // b.printinfo();
-    student c;
-    student d = a;
-    // int n;
-    // cout<<"Enter number of students: "<<endl;
-    // cin>>n;
-    // student arr[n];
-    // for(int i = 0; i<n; i++){
-    //     cout<<endl<<"Student Name: ";
-    //     cin>>arr[i].name;
-    //     cout<<endl<<"Age: ";
-    //     cin>>arr[i].age;
-    //     cout<<endl<<"Gender: ";
-    //     cin>>arr[i].gender;
-    // }
-
-    // for(int i = 0 ; i<n; i++){
-    //     arr[i].printinfo();
-    // }
+    x.number = 1;
+    cout<<x.number<<endl;
+    y.getdata(2,25.5);
+    y.printinfo();
     return 0;
 }

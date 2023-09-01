abcb839f630d9043f8ce0a3cb47c6d5211b29276
Nikunj Maheshwari <nmaheshwari2904@gmail.com>
2023-09-01 17:36:04 +0530

    added functions for reverselist

--- linkedlistclass.cpp
+++ linkedlistclass.cpp
@@ -70,6 +70,40 @@ void insertatposition(node* head, int val, int position){
     p->next = newNode;
 }
 
+
+void deletebegin(node* &head){
+    node* temp = head;
+    if(temp == NULL){
+        cout << "The list is already empty!" << endl;
+        return;
+    }
+    head = temp -> next;
+    free(temp);
+}
+
+void deleteend(node* &head){
+    node* temp = head;
+    if(temp == NULL){
+        cout << "The list is empty!" << endl;
+        return;
+    }
+    while(temp -> next -> next != NULL){
+        temp = temp->next;
+    }
+    free(temp -> next);
+    temp -> next = NULL;
+}
+
+void deletevalue(node* &head, int val){
+    node* temp = head;
+    while(temp -> next -> data != val){
+        temp = temp -> next;
+    }
+    node* del = temp -> next;
+    temp -> next = temp -> next -> next; //we could also use del -> next instead of temp -> next -> next;
+    free(del); //We could also use delete() here instead of free();
+}
+
 void display(node* head){
     node* temp = head;
     while(temp != NULL){
@@ -79,9 +113,44 @@ void display(node* head){
     cout<<"NULL"<<endl;
 }
 
-void sortlist(node* head, int size){
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size - 1; j++){
+void iterativereverse(node* &head)
+{
+    node* prev = NULL;
+    node* curr = head;
+    node* nextpt;
+
+    while(curr != NULL)
+    {
+        nextpt = curr -> next;
+        curr -> next = prev;
+
+        prev = curr;
+        curr = nextpt;
+    }
+    head = prev;
+}
+
+node* recursivereverse(node* &head) //I do not get it..?
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    
+    node* newnode = recursivereverse(head -> next);
+    head -> next -> next = head;
+    head -> next = NULL;
+
+    return newnode;
+}
+
+void sortlist(node* head, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size - 1; j++)
+        {
+
         }
     }
 }
@@ -93,6 +162,14 @@ int main(){
     insertatbegin(head,0);
     insertatposition(head,2,3);
     display(head);
+    iterativereverse(head);
+    display(head);
     traverse(head,2);
+    // deletevalue(head,2);
+    // display(head);
+    // deleteend(head);
+    // display(head);
+    node* newhead = recursivereverse(head); // this bs works but how...?
+    display(newhead);
     return 0;
 }

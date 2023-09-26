f93c50d8c846150011ea80f51ffbeb96946c6b8a
Nikunj Maheshwari <nmaheshwari2904@gmail.com>
2023-09-26 08:50:36 +0530

    something something

--- classandobj.cpp
+++ classandobj.cpp
@@ -1,71 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class student{
+class A
+{
+    int x,y;
     public:
-    string name;
-    int age;
-    bool gender;
-
-    //creating a function of student class
-    void printinfo(){
-        cout<<endl<<"Name: "<<name<<endl;
-        cout<<"Age: "<<age<<endl;
-        if(gender == 0){
-            cout<<"Gender: Female"<<endl;
-        }
-        if(gender == 1){
-            cout<<"Gender: Male"<<endl;
-        }
-    }
-
-    //creating a parameterised constructor function of student class
-    student(string s,int a, int g){
-        cout<<"Parameterised Constructor."<<endl;
-        name = s;
-        age = a;
-        gender = g;
-    }
-
-    //default constructor function of student class
-    student(){
-        cout<<"Default Constructor."<<endl;
-    }
-
-    //copy constructor
-    student(student &a){
-        cout<<"Copy constructor."<<endl;
-        name = a.name;
-        age = a.age;
-        gender = a.gender;
-        cout<<"Name: "<<name<<endl;
-        cout<<"Age: "<<age<<endl;
-        cout<<"Gender: "<<gender<<endl;
-    }
+    friend class B;
+    void getinfo();
 };
 
-int main(){
-    student a("Nikunj",19,1);
-    student b("Neha",19,0);
-    // a.printinfo();
-    // b.printinfo();
-    student c;
-    student d = a;
-    // int n;
-    // cout<<"Enter number of students: "<<endl;
-    // cin>>n;
-    // student arr[n];
-    // for(int i = 0; i<n; i++){
-    //     cout<<endl<<"Student Name: ";
-    //     cin>>arr[i].name;
-    //     cout<<endl<<"Age: ";
-    //     cin>>arr[i].age;
-    //     cout<<endl<<"Gender: ";
-    //     cin>>arr[i].gender;
-    // }
+class B
+{
+    int z;
+    public:
+    void getinfo();
+    int sum(A t);
+};
 
-    // for(int i = 0 ; i<n; i++){
-    //     arr[i].printinfo();
-    // }
+void A :: getinfo()
+{
+    cout<<"Enter X and Y: ";
+    cin >> x >> y;
+}
+
+void B :: getinfo()
+{
+    cout<<"Enter Z: ";
+    cin >> z;
+}
+
+int B :: sum(A t)
+{
+    int sum;
+    sum = z + t.x + t.y;
+    return sum;
+}
+
+int main()
+{
+    A a;
+    B b;
+    a.getinfo();
+    b.getinfo();
+    cout<<"SUM: "<<b.sum(a);
     return 0;
 }
